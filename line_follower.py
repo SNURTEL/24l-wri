122b5d806873cc01d6f7a6dd0f51c362527caba1
bkisly <b.kisly@gmail.com>
2024-05-06 11:52:34 +0200

    Added red and green recognition

--- line_follower.py
+++ line_follower.py
@@ -13,6 +13,10 @@ DEFAULT = 0
 LEFT_TURN = 1
 RIGHT_TURN = 2
 
+BLACK = "Black"
+RED = "Red"
+GREEN = "Green"
+
 
 def main():
 
@@ -25,13 +29,15 @@ def main():
     speed_base = float(sys.argv[1])
     speed_left = speed_base
     speed_right = speed_base
-    backwards_factor = float(sys.argv[3])
+    backwards_factor_base = float(sys.argv[3])
+    backwards_factor_color = backwards_factor_base * 1.5
+    backwards_factor = backwards_factor_base
     slow_speed = float(sys.argv[4])
 
     movement_state = DEFAULT
     previous_movement_state = None
 
-    black_color = "Black"
+    main_color = BLACK
 
     def set_speed_left(new_speed, p=P):
         nonlocal speed_left
@@ -47,27 +53,45 @@ def main():
         previous_movement_state = movement_state
         movement_state = state
 
+    def update_main_color():
+        nonlocal main_color
+        nonlocal sensor_left
+        nonlocal sensor_right
+        nonlocal backwards_factor, backwards_factor_color, backwards_factor_base
+
+        if sensor_left.color_name == RED or sensor_right.color_name == RED:
+            main_color = RED
+            backwards_factor = backwards_factor_color
+        elif sensor_left.color_name == GREEN or sensor_right.color_name == GREEN:
+            main_color = GREEN
+            backwards_factor = backwards_factor_color
+        else:
+            main_color = BLACK
+            backwards_factor = backwards_factor_base
+
     try:
         print("Robot initialized")
 
         while True:
+            update_main_color()
+
             if (
-                sensor_left.color_name == black_color
-                and sensor_right.color_name != black_color and previous_movement_state != RIGHT_TURN
+                sensor_left.color_name == main_color
+                and sensor_right.color_name != main_color and previous_movement_state != RIGHT_TURN
             ):
                 set_speed_left(-backwards_factor * slow_speed)
                 set_speed_right(slow_speed)
                 set_movement_state(LEFT_TURN)
             elif (
-                sensor_right.color_name == black_color
-                and sensor_left.color_name != black_color and previous_movement_state != LEFT_TURN
+                sensor_right.color_name == main_color
+                and sensor_left.color_name != main_color and previous_movement_state != LEFT_TURN
             ):
                 set_speed_right(-backwards_factor * slow_speed)
                 set_speed_left(slow_speed)
                 set_movement_state(RIGHT_TURN)
             elif (
-                sensor_left.color_name != black_color
-                and sensor_right.color_name != black_color
+                sensor_left.color_name != main_color
+                and sensor_right.color_name != main_color
             ):
                 set_speed_left(speed_base)
                 set_speed_right(speed_base)
@@ -75,9 +99,6 @@ def main():
 
             motor_left.on(speed_left)
             motor_right.on(speed_right)
-
-            # print("Speed left: " + str(speed_left))
-            # print("Speed right: " + str(speed_right))
     finally:
         motor_left.off()
         motor_right.off()
